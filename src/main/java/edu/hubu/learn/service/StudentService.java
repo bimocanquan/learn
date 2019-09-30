8b5424482ad63223fd104170c0708a49bb91ab11
bimocanquan <14577766258@.com>
2019-09-30 20:47:22 +0800

    date

--- src/main/java/edu/hubu/learn/service/StudentService.java
+++ src/main/java/edu/hubu/learn/service/StudentService.java
@@ -1,5 +1,7 @@
 package edu.hubu.learn.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,4 +17,12 @@ public class StudentService {
     public Student getUser(Long id) {
         return studentDao.findById(id).get();
     }
+
+	public List<Student> getStudent(Long id) {
+		return null;
+	}
+
+	public List<Student> getStudents() {
+		return null;
+	}
 }
---
+++ src/main/resources/templates/students.ftl
@@ -0,0 +1,12 @@
+<html>
+<head></head>
+<body>
+<table>
+<#list students as student123>
+<tr>
+<td>${student123.studentname}</td><td>${student123.password}</td>
+</tr>
+</#list>
+</table>
+</body>
+</html>
